325c11a9a4d6436c89d4c1e19356845aea8fb315
repac <repac37@gmail.com>
2018-07-06 16:10:52 +0200

    Repository

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -7,17 +7,34 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using DutchTreat.Services;
+using DutchTreat.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace DutchTreat
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DutchContext>(cfg =>
+            {
+                cfg.UseSqlServer(_config.GetConnectionString("DucthConnectionString"));
+            });
+           
             services.AddTransient<IMailService, NullMailService>();
+            services.AddTransient<DutchSeeder>();
             // Support for real mail service
+
+            services.AddScoped<IDutchRepository, DutchRepository>();
 
             services.AddMvc();
         }
@@ -44,6 +61,16 @@ namespace DutchTreat
                     "{controller}/{action}/{id?}",
                     new { controller = "App", Action = "Index" });
             });
+
+            if (env.IsDevelopment())
+            {
+                //Seed database
+                using (var scope = app.ApplicationServices.CreateScope())
+                {
+                    var seeder = scope.ServiceProvider.GetService<DutchSeeder>();
+                    seeder.Seed();
+                }
+            }
         }
     }
 }
